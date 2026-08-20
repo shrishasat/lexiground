f3df553a3a0b8f3fbf418cb8628f48e87689cf2e
Shrisha <166478099+shrishasat@users.noreply.github.com>
2026-08-20 15:07:02 +0100

    Implement RidgeEstimator class for regression

--- src/lexiground/models.py
+++ src/lexiground/models.py
@@ -1,1 +1,24 @@
+import joblib
+import numpy as np
+from sklearn.linear_model import Ridge
 
+
+class RidgeEstimator:
+
+    def __init__(self, alpha=1.0):
+        self.alpha = alpha
+        self.model = Ridge(alpha=alpha)
+
+    def fit(self, X, y):
+        self.model.fit(X, y)
+        return self
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+    def save(self, path):
+        joblib.dump(self.model, path)
+
+    def load(self, path):
+        self.model = joblib.load(path)
+        return self
